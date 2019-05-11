55baa15844822cf5a6849a0698f95670a5f06b1e
Arsen Aganesov <arsen.aganesov@gmail.com>
2019-05-11 21:50:19 +0300

    Two new functions:
    * "расписание" - use this word, if you want to now schedule
    
    * "урок" - [work in progress, the function partially works, all functionality is not yet implemented]

--- vk_bot.py
+++ vk_bot.py
@@ -1,12 +1,34 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+import datetime as dt
+filename = "ID_Users.txt"
+
+
+def add_id(user_id):
+    status = True
+    with open(filename, "r+") as f:
+        for x in f.readlines():
+            if x.strip() in str(user_id):
+                status = False
+                break
+        if status:
+            f.write(str(user_id))
 
 
 def write_msg(user_id, message):
-    vk.method('message.send', {'user_id':user_id, 'message':message,
-                               'random_id':0})
+    vk.method('messages.send', {'user_id': user_id, 'message': message,
+                                'random_id': 0})
 
-token = "5a1c5dd1b0b83ec50812fd33576df54ffcfb916d2386d9771d879b54de9dd126a0a078fc346a023fd98a2"
+
+def send_schedule(user_id):
+    filename = "schedule.txt"
+    message = ''
+    with open(filename, "r") as f:
+        for line in f.readlines():
+            message += "    "+line
+    write_msg(user_id, message)
+
+token = "0d32f1db60514159733d06bddc33fac80a6636d535994801ac8c9d45cf361f66e8a8caa9180889b8e02f9"
 
 vk = vk_api.VkApi(token=token)
 
@@ -14,9 +36,10 @@ longpoll = VkLongPoll(vk)
 
 while True:
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print("Text message: " + str(event.text))
-            response = event.text.lower()
-            if event.from_user and not event.from_me:
-                user_id = event.user_id
-                write_msg(user_id, 'Твой ID: ' + str(user_id))
+        if event.type == VkEventType.MESSAGE_NEW and not event.from_me:
+            add_id(str(event.user_id))
+            if event.text.lower() == "расписание":
+                send_schedule(event.user_id)
+            if event.text.lower() == "урок":
+                if dt.datetime.today().isoweekday() in (6, 7):
+                    write_msg(event.user_id, "Сегодня уроков нет :)")
