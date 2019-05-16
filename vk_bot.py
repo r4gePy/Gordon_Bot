8530043f64e15455e4a964ee4d14ccf5b9be2d3b
Arsen Aganesov <arsen.aganesov@gmail.com>
2019-05-16 09:28:58 +0300

    Now the bot is finished, it is ready to work
    *Function "уроки" ready to work
    
    *Now we have data base, to maintain the number of users who wrote the bot

--- vk_bot.py
+++ vk_bot.py
@@ -2,19 +2,29 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import datetime as dt
 import time
+import sqlite3
 
-filename = "ID_Users.txt"
+conn = sqlite3.connect("users_id.db")
+cursor = conn.cursor()
 
 
-def add_id(user_id):
-    status = 1
-    with open(filename, "r+") as f:
-        for x in f.readlines():
-            if x.strip() in str(user_id):
-                status = 0
-                break
-        if status == 1:
-            f.write(str(user_id))
+def get_info(user_id):
+    return vk.method("users.get", {"user_ids":user_id})
+
+
+def add_in_table(id, first_name, second_name):
+    info = [(id, first_name, second_name)]
+    sql = "SELECT ID FROM USERS_INFO"
+    cursor.execute(sql)
+    ids = cursor.fetchall()
+    rez = True
+    for usr in ids:
+        if usr[0] == id:
+            rez = False
+            break
+    if rez:
+        cursor.executemany("INSERT INTO main.USERS_INFO VALUES (?, ?, ?)", info)
+        conn.commit()
 
 
 def write_msg(user_id, message):
@@ -39,12 +49,15 @@ longpoll = VkLongPoll(vk)
 while True:
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and not event.from_me:
-            add_id(str(event.user_id))
+            user_info = get_info(event.user_id)
+            add_in_table(event.user_id, user_info[0]["first_name"], user_info[0]["last_name"])
+            write_msg(event.user_id, "Привет, " + user_info[0]["first_name"] + "!")
             if event.text.lower() == "расписание":
                 send_schedule(event.user_id)
             if event.text.lower() == "урок":
                 if dt.datetime.today().isoweekday() in (6, 7):
                     write_msg(event.user_id, "Сегодня уроков нет :)")
+# Высчитывание кол-ва минут до начала/конца урока
                 else:
                     time_local = time.localtime()
                     minutes = (60*time_local.tm_hour) + time_local.tm_min
@@ -72,5 +85,37 @@ while True:
                         write_msg(event.user_id,
                                   "До конца третьего урока(мин.) :" +
                                   str(655-minutes))
-                    elif 500:
-                        pass
+                    elif 655 < minutes < 670:
+                        write_msg(event.user_id,
+                                  "До начала четвертого урока(мин.) :" +
+                                  str(670-minutes))
+                    elif 670 < minutes < 710:
+                        write_msg(event.user_id,
+                                  "До конца четвертого урока(мин.) :" +
+                                  str(710-minutes))
+                    elif 710 < minutes < 725:
+                        write_msg(event.user_id,
+                                  "До начала пятого урока(мин.) :" +
+                                  str(725-minutes))
+                    elif 725 < minutes < 765:
+                        write_msg(event.user_id,
+                                  "До конца пятого урока(мин.) :" +
+                                  str(765-minutes))
+                    elif 765 < minutes < 775:
+                        write_msg(event.user_id,
+                                  "До начала шестого урока(мин.) :" +
+                                  str(775-minutes))
+                    elif 775 < minutes < 815:
+                        write_msg(event.user_id,
+                                  "До конца шестого урока(мин.) :" +
+                                  str(815-minutes))
+                    elif 815 < minutes < 825:
+                        write_msg(event.user_id,
+                                  "До начала седьмого урока(мин.) :" +
+                                  str(825-minutes))
+                    elif 825 < minutes < 865:
+                        write_msg(event.user_id,
+                                  "До конца седьмого урока(мин.) :" +
+                                  str(865-minutes))
+                    else:
+                        write_msg(event.user_id, "Уроки закончились.")
