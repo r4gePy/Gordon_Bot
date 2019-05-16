36c9f47b7e175b358a090b30011a09b34bba9b3d
Arsen Aganesov <arsen.aganesov@gmail.com>
2019-05-16 09:59:19 +0300

    SF

--- file.py
+++ file.py
@@ -1,16 +1,18 @@
-filename = "file.txt"
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+import sqlite3
+
+token = "0d32f1db60514159733d06bddc33fac80a6636d535994801ac8c9d45cf361f66e8a8caa9180889b8e02f9"
+
+vk = vk_api.VkApi(token=token)
+
+longpoll = VkLongPoll(vk)
+conn = sqlite3.connect("users_id.db")
+cursor = conn.cursor()
 
 
-def add_id(user_id):
-    status = True
-    with open(filename, "r+") as f:
-        for x in f.readlines():
-            print(x.strip())
-            if x.strip() in str(user_id):
-                status = False
-                break
-        if status:
-            f.write(str(user_id))
-
-
-add_id(1231)
+sql = "SELECT ID FROM USERS_INFO"
+cursor.execute(sql)
+ids = cursor.fetchall()
+for usr in ids:
+    print(usr[0])
