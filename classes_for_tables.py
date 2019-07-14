8b70cf2b13460b087d34fd54b2805624c2ba2aab
Arsen Aganesov <arsen.aganesov@gmail.com>
2019-07-14 13:30:27 +0300

    File for optimisation main file

--- classes_for_tables.py
+++ classes_for_tables.py
@@ -1,7 +1,14 @@
 from peewee import *
 
-db = SqliteDatabase("USERS.db")
+db_info = SqliteDatabase("USERS.db")
 db_hw = SqliteDatabase("HOMEWORK.db")
+
+dict_monday = {
+    1: "Математика",
+    2: "Русский язык",
+    3: "Литература",
+    4: "English"
+}
 
 
 class MONDAY(Model):
@@ -12,12 +19,43 @@ class MONDAY(Model):
         database = db_hw
 
 
-class INFO(Model):
-    ID = IntegerField()
-    FNAME = TextField()
-    SNAME = TextField()
-    ST = IntegerField()
+class TUESDAY(Model):
+    Lesson = TextField()
+    HW = TextField()
 
     class Meta:
-        database = db
+        database = db_hw
+
+
+class WEDNESDAY(Model):
+    Lesson = TextField()
+    HW = TextField()
+
+    class Meta:
+        database = db_hw
+
+
+class THURSDAY(Model):
+    Lesson = TextField()
+    HW = TextField()
+
+    class Meta:
+        database = db_hw
+
+
+class FRIDAY(Model):
+    Lesson = TextField()
+    HW = TextField()
+
+    class Meta:
+        database = db_hw
+
+
+class INFO(Model):
+    ID_VK = IntegerField()
+    F_NAME = TextField()
+    S_NAME = TextField()
+
+    class Meta:
+        database = db_info
 
