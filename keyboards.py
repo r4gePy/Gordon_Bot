0d70ae05479d207dbb95e306e6784c01ae6d2f59
Arsen <arsen.aganesov@gmail.com>
2021-02-02 23:54:55 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -30,3 +30,53 @@ KEYBOARD_START = {
 
     ]
 }
+
+
+KEYBOARD_NUMBERS_DAYS = {
+    "one_time": True,
+    "buttons": [
+        [{
+            "action": {
+                "type": "text",
+                "payload": "{\"button\": \"1 \"}",
+                "label": "1",
+            },
+            "color": "secondary"
+        },
+            {
+            "action": {
+                "type": "text",
+                "payload": "{\"button\": \"2 \"}",
+                "label": "2",
+            },
+            "color": "secondary"
+            },
+        ],
+        [{
+            "action": {
+                "type": "text",
+                "payload": "{\"button\": \"3 \"}",
+                "label": "3",
+            },
+            "color": "secondary"
+        },
+            {
+            "action": {
+                "type": "text",
+                "payload": "{\"button\": \"4 \"}",
+                "label": "4",
+            },
+            "color": "secondary"
+            },
+        ],
+        [{
+            "action": {
+                "type": "text",
+                "payload": "{\"button\": \"5 \"}",
+                "label": "5",
+            },
+            "color": "secondary"
+            },
+        ]
+    ]
+}
